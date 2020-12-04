5afe39054ecfd074dd2008204d3c50f080acde15
Jacobi <1183710125@stu.hit.edu.cn>
2020-12-04 21:42:08 +0800

    通知界面

--- app/src/main/java/com/example/demo/activity/NotificationActivity.java
+++ app/src/main/java/com/example/demo/activity/NotificationActivity.java
@@ -1,8 +1,11 @@
 package com.example.demo.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.demo.R;
 import com.example.demo.ui.notifications.notification.NotificationFragment;
@@ -18,5 +21,20 @@ public class NotificationActivity extends AppCompatActivity {
                     .replace(R.id.notification_container, NotificationFragment.newInstance())
                     .commitNow();
         }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
